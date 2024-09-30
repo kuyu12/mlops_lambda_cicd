96d1818ac416450a2d7481075b28bc67803e2082
Aviad Rozenhek <aviadr1@gmail.com>
2024-09-30 19:34:26 +0300

    test1

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,6 +2,8 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+
+    print("test1")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
